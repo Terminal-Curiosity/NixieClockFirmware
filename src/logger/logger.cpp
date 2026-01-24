b3b1bff2a5d29540c96fdbc357a1be0e03a772c8
Terminal-Curiosity <137018407+Terminal-Curiosity@users.noreply.github.com>
2026-01-25 00:38:41 +1030

    overload logger to allow 2 arguments

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -12,10 +12,18 @@ void logInfo(const char* message) {
     Serial.println(message);
 }
 
+void logInfo(const char* msg1, const char* msg2)
+{
+    Serial.print("[INFO] ");
+    Serial.print(msg1);
+    Serial.println(msg2);
+}
+
 void logWarning(const char* message) {
     Serial.print("[WARNING] ");
     Serial.println(message);
 }
+
 void logError(const char* message) {
     Serial.print("[ERROR] ");
     Serial.println(message);
--- src/logger/logger.h
+++ src/logger/logger.h
@@ -4,5 +4,6 @@ class String;
 bool loggerInit(void);
 
 void logInfo(const char* message);
+void logInfo(const char* msg1, const char* msg2);
 void logWarning(const char* message);
 void logError(const char* message);
